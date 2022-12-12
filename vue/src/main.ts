ff449c46d4f82693029aa8ce7344ba0391eab543
twill <twill1@gmx.com>
2022-12-11 18:43:34 -0600

    Add vue router and vuex

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -3,9 +3,11 @@ import App from "@/App.vue";
 import "./assets/tailwind.css";
 
 import axios from "axios";
+import router from "./router";
+import store from "./store";
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 // axios.defaults.headers.common["Content-Type"] = "application/json";
 // axios.defaults.headers.common["Accept"] = "application/json";
 axios.defaults.withCredentials = true;
 
-createApp(App).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
