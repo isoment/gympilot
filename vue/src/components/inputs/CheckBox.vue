8dbddf402a8de74a86c698f2807b7ee594db9a47
twill <twill1@gmx.com>
2023-04-26 00:33:18 -0500

    Start checkbox component

---
+++ vue/src/components/inputs/CheckBox.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex items-center">
+    <input
+      v-model="isChecked"
+      type="checkbox"
+      class="w-5 h-5"
+      @change="onChange"
+    />
+    <label class="ml-2 font-mono font-bold">{{ label }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "CheckBox",
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  emits: ["update:modelValue"],
+
+  setup(props, { emit }) {
+    const isChecked = ref(props.modelValue);
+
+    const onChange = () => {
+      emit("update:modelValue", isChecked.value);
+    };
+
+    return {
+      onChange,
+      isChecked,
+    };
+  },
+});
+</script>
---
+++ vue/src/views/ComponentTest.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="flex justify-around">
+      <CheckBox v-model="checkboxData" label="Test Check" />
+      <CheckBox v-model="checkboxData" label="Test Check" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import CheckBox from "@/components/inputs/CheckBox.vue";
+
+export default defineComponent({
+  name: "ComponentTest",
+
+  components: { CheckBox },
+
+  setup() {
+    const checkboxData = ref(true);
+
+    return { checkboxData };
+  },
+});
+</script>
