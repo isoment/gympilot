6ed62875bb14cfaef28f4320d120b014a8f09dd8
twill <twill1@gmx.com>
2023-07-02 22:17:45 -0500

    Install typescript, nodedev packages and jest

--- api/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '@exmpl/(.*)': '<rootDir>/src/$1',
-    '^@base/(.*)$': '<rootDir>/src/$1',
-    '^@api/(.*)$': '<rootDir>/src/api/$1',
-  },
-};
