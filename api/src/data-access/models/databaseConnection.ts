c900984a6af8b1cef366a845ad3334bc2c40841b
twill <twill1@gmx.com>
2023-07-17 22:55:53 -0500

    Add database connection

---
+++ api/src/data-access/models/databaseConnection.ts
@@ -0,0 +1,30 @@
+import { Sequelize, Options } from "sequelize";
+import { logger } from "../../logger/logger";
+import database from "../../config/database";
+import { appConfig } from "../../config/app";
+
+// ️️️Best Practice: Keep a singleton DB connection pool in a process
+let dbConnection: Sequelize;
+
+export default function getDbConnection() {
+  if (!dbConnection) {
+    const name = appConfig.node === "test" ? database.test_database : database.database;
+    dbConnection = new Sequelize(name, database.user, database.password, {
+      port: 54320,
+      dialect: "postgres",
+      benchmark: true,
+      logging: (sql: string, duration?: number) => {
+        logger.info(`Sequelize operation was just executed in ${duration} ms with sql: ${sql}`);
+      },
+      logQueryParameters: true,
+      pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    });
+  }
+
+  return dbConnection;
+}
---
+++ api/test/unit/example.test.ts
@@ -0,0 +1,6 @@
+describe("addition", () => {
+  it("adds 1 and 1", () => {
+    const sum = 1 + 1;
+    expect(sum).toBe(2);
+  });
+});
