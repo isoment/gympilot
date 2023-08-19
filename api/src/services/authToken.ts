361010fbbc20e2738dd6f1f7918f8a99707dd040
twill <twill1@gmx.com>
2023-08-19 12:49:02 -0500

    Update generate method for auth tokens

--- api/src/services/authToken.ts
+++ api/src/services/authToken.ts
@@ -1,10 +1,24 @@
-import { UserWithRoles } from "@base/data-access/repositories/userRepository";
-import { appConfig } from "../config/app";
 import jwt from "jsonwebtoken";
+import { appConfig } from "@base/config/app";
+import { logger } from "@base/logger/logger";
 
-const generate = (payload: UserWithRoles) => {
-  const token = jwt.sign(payload, appConfig.jwtPrivateKey);
-  return token;
+/**
+ * We can generate a jwt token asynchronously by using a callback
+ * @param payload to include in the token.
+ * @param options to set for the token, encryption, expiration etc.
+ * @returns Promise<string>
+ */
+const generate = async (payload: object, options: jwt.SignOptions = {}): Promise<string> => {
+  return new Promise<string>((resolve, reject) => {
+    jwt.sign(payload, appConfig.jwtPrivateKey, options, (error: Error | null, token?: string) => {
+      if (error) {
+        logger.error(`Failed to generate a jwt token.`, error);
+        reject(error);
+      } else {
+        resolve(token!);
+      }
+    });
+  });
 };
 
 export default {
