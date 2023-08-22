800caa12a85e2f7c40788ef1600fd39ac820cb14
twill <twill1@gmx.com>
2023-08-21 20:31:43 -0500

    Create migration and model for password resets

---
+++ api/src/data-access/models/passwordReset.ts
@@ -0,0 +1,36 @@
+import getDbConnection from "./databaseConnection";
+import { DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional } from "sequelize";
+
+export interface PasswordResetFields extends Model<InferAttributes<PasswordResetFields>, InferCreationAttributes<PasswordResetFields>> {
+  id: CreationOptional<number>;
+  email: string;
+  token: string;
+  expires: Date;
+}
+
+const PasswordReset = getDbConnection().define<PasswordResetFields>(
+  "PasswordReset",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    expires: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  { tableName: "password_resets" },
+);
+
+export default PasswordReset;
