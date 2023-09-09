d8a2ccf80f14685dd7a33aa275e632f1aa326ce4
twill <twill1@gmx.com>
2023-09-09 00:38:06 -0500

    Test register request validation

--- api/test/integration/authController/register.test.ts
+++ api/test/integration/authController/register.test.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import _ from "lodash";
 
 import { startWebServer, stopWebServer } from "../../../src/server/server";
 import model from "../../../src/data-access/models";
@@ -30,6 +31,63 @@ afterEach(async () => {
 });
 
 describe("POST /api/auth/register", () => {
+  const createRequestBody = (params = {}) => {
+    return {
+      first_name: "Another",
+      last_name: "Guy",
+      email: "user22@test.com",
+      password: "password",
+      password_verify: "password",
+      ...params,
+    };
+  };
+
+  it("requires a first name field", async () => {
+    const body = _.omit(createRequestBody(), "first_name");
+    const response = await axiosAPIClient.post(endpoint, body);
+    expect(response.status).toBe(422);
+    expect(response.data).toHaveProperty("first_name");
+  });
+
+  it("requires a last name field", async () => {
+    const body = _.omit(createRequestBody(), "last_name");
+    const response = await axiosAPIClient.post(endpoint, body);
+    expect(response.status).toBe(422);
+    expect(response.data).toHaveProperty("last_name");
+  });
+
+  it("requires an email field", async () => {
+    const body = _.omit(createRequestBody(), "email");
+    const response = await axiosAPIClient.post(endpoint, body);
+    expect(response.status).toBe(422);
+    expect(response.data).toHaveProperty("email");
+  });
+
+  it("requires a password field", async () => {
+    const body = _.omit(createRequestBody(), "password");
+    const response = await axiosAPIClient.post(endpoint, body);
+    expect(response.status).toBe(422);
+    expect(response.data).toHaveProperty("password");
+  });
+
+  it("requires a password verify field", async () => {
+    const body = _.omit(createRequestBody(), "password_verify");
+    const response = await axiosAPIClient.post(endpoint, body);
+    expect(response.status).toBe(422);
+    expect(response.data).toHaveProperty("password_verify");
+  });
+
+  it("requires password and password verify to match", async () => {
+    const body = createRequestBody({
+      password: "password",
+      password_verify: "wordpass",
+    });
+    const response = await axiosAPIClient.post(endpoint, body);
+    expect(response.status).toBe(422);
+    console.log(response);
+    expect(response.data).toHaveProperty("password_verify");
+  });
+
   it("returns a 422 response if there is a user registered with the given email", async () => {
     await model.User.create({
       first_name: "Test",
@@ -38,13 +96,7 @@ describe("POST /api/auth/register", () => {
       password: "password",
     });
 
-    const response = await axiosAPIClient.post(endpoint, {
-      first_name: "Another",
-      last_name: "Guy",
-      email: "user22@test.com",
-      password: "password",
-      password_verify: "password",
-    });
+    const response = await axiosAPIClient.post(endpoint, createRequestBody());
 
     expect(response.status).toBe(422);
   });
