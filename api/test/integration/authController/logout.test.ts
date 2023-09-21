bae97e1c7088adc77140ad6e6887dd3e34948dfb
twill <twill1@gmx.com>
2023-09-20 21:03:40 -0500

    Finish logout test

--- api/test/integration/authController/logout.test.ts
+++ api/test/integration/authController/logout.test.ts
@@ -3,6 +3,9 @@ import axios, { AxiosInstance } from "axios";
 import { startWebServer, stopWebServer } from "../../../src/server/server";
 import model from "../../../src/data-access/models";
 import roleHelper from "../..//testing/helpers/roles";
+import userHelper from "../../testing/helpers/users";
+import authToken from "../../../src/services/authToken";
+import * as refreshTokenStore from "../../../src/data-access/memory-store/refreshTokenStore";
 
 const endpoint = "/api/auth/logout";
 let axiosAPIClient: AxiosInstance;
@@ -37,5 +40,66 @@ describe("POST /api/auth/logout", () => {
   it("returns a success response status when no refresh token cookie is provided", async () => {
     const response = await axiosAPIClient.post(endpoint);
     expect(response.status).toBe(200);
+  });
+
+  it("removes an existing refresh token from the memory store", async () => {
+    // Create a user and a cookie with the refresh token
+    const user = await userHelper.createUser();
+    const refreshToken = await authToken.create(user!.toJSON(), { expiresIn: 99999 });
+    const cookie = `refresh_token=${refreshToken}; Path=/; HttpOnly;`;
+
+    // Put the refresh token in the memory store
+    await refreshTokenStore.set(user!.id, refreshToken, 99999);
+
+    const response = await axiosAPIClient.post(
+      endpoint,
+      {},
+      {
+        headers: {
+          Cookie: cookie,
+        },
+      },
+    );
+
+    const tokenFromStore = await refreshTokenStore.get(user!.id);
+    expect(response.status).toBe(200);
+    expect(tokenFromStore).toBeFalsy();
+  });
+
+  it("returns success response if the token is invalid", async () => {
+    const cookie = `refresh_token=invalid.some-invalid.token123456; Path=/; HttpOnly;`;
+
+    const response = await axiosAPIClient.post(
+      endpoint,
+      {},
+      {
+        headers: {
+          Cookie: cookie,
+        },
+      },
+    );
+
+    expect(response.status).toBe(200);
+  });
+
+  it("returns internal error response if a service throws an exception", async () => {
+    jest.spyOn(refreshTokenStore, "remove").mockRejectedValue(new Error("Simulated refreshTokenStore error"));
+
+    // Create a valid refresh token cookie
+    const user = await userHelper.createUser();
+    const refreshToken = await authToken.create(user!.toJSON(), { expiresIn: 99999 });
+    const cookie = `refresh_token=${refreshToken}; Path=/; HttpOnly;`;
+
+    const response = await axiosAPIClient.post(
+      endpoint,
+      {},
+      {
+        headers: {
+          Cookie: cookie,
+        },
+      },
+    );
+
+    expect(response.status).toBe(500);
   });
 });
