2f130bfcb7426d9bfa62156b7a8f98c7a4b79e43
twill <twill1@gmx.com>
2024-02-02 12:50:07 -0600

    Finish SingleModal component test

--- vue/tests/unit/components/modals/SingleModal.test.ts
+++ vue/tests/unit/components/modals/SingleModal.test.ts
@@ -3,20 +3,34 @@ import SingleModal from "@/components/modals/SingleModal.vue";
 import ResizeObserver from "resize-observer-polyfill";
 import TestModalSlot from "../../../setup/test-components/TestModalSlot.vue";
 
+jest.useFakeTimers();
+
 // Use ResizeObserver ponyfill.
 global.ResizeObserver = ResizeObserver;
 
 describe("SingleModal", () => {
-  const createSlotContent = (): string => {
-    return `<div class="relative inline-block overflow-hidden align-bottom transition-all transform" data-test="slot-content"><input type="text" /></div>`;
-  };
+  it("displays the passed-in component in the slot", async () => {
+    const wrapper = mount(SingleModal, {
+      props: { modelValue: true },
+      slots: {
+        default: TestModalSlot,
+      },
+    });
+
+    // Wait for the next tick to ensure any transitions are completed
+    await wrapper.vm.$nextTick();
+
+    // Use findComponent with the name of the test component
+    const testComponent = wrapper.findComponent({ name: "TestModalSlot" });
+    expect(testComponent.exists()).toBe(true);
+  });
 
   describe("the modelValue prop controls the modal state", () => {
     it("the modal is shown when the modelValue is true", async () => {
       const wrapper = mount(SingleModal, {
         props: { modelValue: true },
         slots: {
-          default: createSlotContent(),
+          default: TestModalSlot,
         },
       });
 
@@ -30,7 +44,7 @@ describe("SingleModal", () => {
       const wrapper = mount(SingleModal, {
         props: { modelValue: false },
         slots: {
-          default: createSlotContent(),
+          default: TestModalSlot,
         },
       });
 
@@ -39,8 +53,10 @@ describe("SingleModal", () => {
       const modalOverlay = wrapper.findComponent({ name: "DialogOverlay" });
       expect(modalOverlay.exists()).toBe(false);
     });
+  });
 
-    it("displays the passed-in component in the slot", async () => {
+  describe("closing the modal", () => {
+    it("emits and event", async () => {
       const wrapper = mount(SingleModal, {
         props: { modelValue: true },
         slots: {
@@ -48,12 +64,13 @@ describe("SingleModal", () => {
         },
       });
 
-      // Wait for the next tick to ensure any transitions are completed
       await wrapper.vm.$nextTick();
 
-      // Use findComponent with the name of the test component
-      const testComponent = wrapper.findComponent({ name: "TestModalSlot" });
-      expect(testComponent.exists()).toBe(true);
+      await wrapper.findComponent({ name: "DialogOverlay" }).trigger("click");
+
+      // Assert that the "update:modelValue" event was emitted with the correct value
+      expect(wrapper.emitted("update:modelValue")).toBeTruthy();
+      expect(wrapper.emitted("update:modelValue")![0]).toEqual([false]);
     });
   });
 });
