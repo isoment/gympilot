764770b3c4a7640a75a1efd798df292f24feecf3
twill <twill1@gmx.com>
2024-02-03 23:44:47 -0600

    Refactor to DashboardNavProfile menu component

--- vue/src/components/navigation/DashboardTopNavbar.vue
+++ vue/src/components/navigation/DashboardTopNavbar.vue
@@ -53,105 +53,29 @@
         </button>
 
         <!-- Profile dropdown -->
-        <Menu as="div" class="relative ml-3">
-          <div>
-            <MenuButton
-              class="flex items-center max-w-xs text-sm bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
-            >
-              <span class="sr-only">Open user menu</span>
-              <img
-                class="w-8 h-8 rounded-full"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-            </MenuButton>
-          </div>
-          <transition
-            enter-active-class="transition duration-100 ease-out"
-            enter-from-class="transform scale-95 opacity-0"
-            enter-to-class="transform scale-100 opacity-100"
-            leave-active-class="transition duration-75 ease-in"
-            leave-from-class="transform scale-100 opacity-100"
-            leave-to-class="transform scale-95 opacity-0"
-          >
-            <MenuItems
-              class="absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-            >
-              <MenuItem
-                v-for="item in items"
-                :key="item.name"
-                v-slot="{ active }"
-                @click="menuItemClick(item)"
-              >
-                <a
-                  :href="item.href"
-                  :class="[
-                    active ? 'bg-gray-100' : '',
-                    'block px-4 py-2 text-sm text-gray-700',
-                  ]"
-                  >{{ item.name }}</a
-                >
-              </MenuItem>
-            </MenuItems>
-          </transition>
-        </Menu>
+        <DashboardNavProfile />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { PropType, defineComponent } from "vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
-import { key } from "@/store";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
-import { DashboardTopNavItem } from "@/config/types";
-import { ADD_TOAST, LOGOUT_USER } from "@/store/constants";
+import { defineComponent } from "vue";
+import DashboardNavProfile from "@/components/menus/DashboardNavProfile.vue";
 
 export default defineComponent({
   name: "DashboardTopNavbar",
 
-  components: {
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuItems,
-  },
-
-  props: {
-    items: {
-      type: Array as PropType<DashboardTopNavItem[]>,
-      required: true,
-    },
-  },
+  components: { DashboardNavProfile },
 
   emits: ["openSidebar"],
 
-  setup(props, { emit }) {
-    const router = useRouter();
-    const store = useStore(key);
-
+  setup(_, { emit }) {
     const sidebarOpen = (): void => {
       emit("openSidebar", true);
     };
 
-    const menuItemClick = async (item: DashboardTopNavItem) => {
-      if (item.value === "logout") {
-        logout();
-      }
-    };
-
-    const logout = async () => {
-      await store.dispatch(LOGOUT_USER);
-      router.push({ name: "login" });
-      store.dispatch(ADD_TOAST, {
-        type: "success",
-        message: "You are now logged out",
-      });
-    };
-
-    return { sidebarOpen, menuItemClick };
+    return { sidebarOpen };
   },
 });
 </script>
