e627b5c9f7e70dd3d3a334d6815b058c9253b0f4
twill <twill1@gmx.com>
2024-02-07 23:42:53 -0600

    Styling onboarding layout

--- vue/src/layouts/OnboardingLayout.vue
+++ vue/src/layouts/OnboardingLayout.vue
@@ -1,5 +1,159 @@
 <template>
-  <div>Onboarding</div>
+  <div>
+    <div class="flex justify-center w-full">
+      <div
+        class="w-full max-w-6xl mx-2 mt-20 border rounded-lg shadow-sm md:w-8/10 lg:pt-2 border-slate-200"
+      >
+        <div class="mt-4 ml-8 text-left">
+          <h3
+            class="font-sans text-4xl font-black tracking-wide text-slate-800"
+          >
+            Welcome to GymPilot
+          </h3>
+          <h5 class="mt-2 text-slate-500">
+            Let's take a few minutes to setup your account
+          </h5>
+        </div>
+
+        <nav class="p-6 m-2" aria-label="Progress">
+          <ol role="list" class="overflow-hidden">
+            <li
+              v-for="(step, stepIdx) in steps"
+              :key="step.name"
+              :class="[stepIdx !== steps.length - 1 ? 'pb-10' : '', 'relative']"
+            >
+              <template v-if="step.status === 'complete'">
+                <div
+                  v-if="stepIdx !== steps.length - 1"
+                  class="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-indigo-600"
+                  aria-hidden="true"
+                />
+                <a :href="step.href" class="relative flex items-start group">
+                  <span class="flex items-center h-9">
+                    <span
+                      class="relative z-10 flex items-center justify-center w-8 h-8 bg-indigo-600 rounded-full group-hover:bg-indigo-800"
+                    >
+                      <font-awesome-icon
+                        :icon="['fa', 'check']"
+                        class="text-sm text-white"
+                      >
+                      </font-awesome-icon>
+                    </span>
+                  </span>
+                  <span class="flex flex-col items-start min-w-0 ml-4">
+                    <span
+                      class="text-xs font-semibold tracking-wide uppercase"
+                      >{{ step.name }}</span
+                    >
+                    <span class="text-sm text-gray-500">{{
+                      step.description
+                    }}</span>
+                  </span>
+                </a>
+              </template>
+              <template v-else-if="step.status === 'current'">
+                <div
+                  v-if="stepIdx !== steps.length - 1"
+                  class="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
+                  aria-hidden="true"
+                />
+                <a
+                  :href="step.href"
+                  class="relative flex items-start group"
+                  aria-current="step"
+                >
+                  <span class="flex items-center h-9" aria-hidden="true">
+                    <span
+                      class="relative z-10 flex items-center justify-center w-8 h-8 bg-white border-2 border-indigo-600 rounded-full"
+                    >
+                      <span class="h-2.5 w-2.5 bg-indigo-600 rounded-full" />
+                    </span>
+                  </span>
+                  <span class="flex flex-col items-start min-w-0 ml-4">
+                    <span
+                      class="text-xs font-semibold tracking-wide text-indigo-600 uppercase"
+                      >{{ step.name }}</span
+                    >
+                    <span class="text-sm text-gray-500">{{
+                      step.description
+                    }}</span>
+                  </span>
+                </a>
+              </template>
+              <template v-else>
+                <div
+                  v-if="stepIdx !== steps.length - 1"
+                  class="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
+                  aria-hidden="true"
+                />
+                <a :href="step.href" class="relative flex items-start group">
+                  <span class="flex items-center h-9" aria-hidden="true">
+                    <span
+                      class="relative z-10 flex items-center justify-center w-8 h-8 bg-white border-2 border-gray-300 rounded-full group-hover:border-gray-400"
+                    >
+                      <span
+                        class="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300"
+                      />
+                    </span>
+                  </span>
+                  <span class="flex flex-col items-start min-w-0 ml-4">
+                    <span
+                      class="text-xs font-semibold tracking-wide text-gray-500 uppercase"
+                      >{{ step.name }}</span
+                    >
+                    <span class="text-sm text-gray-500">{{
+                      step.description
+                    }}</span>
+                  </span>
+                </a>
+              </template>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+const steps = [
+  {
+    name: "Create account",
+    description: "Vitae sed mi luctus laoreet.",
+    href: "#",
+    status: "complete",
+  },
+  {
+    name: "Profile information",
+    description: "Cursus semper viverra facilisis et et some more.",
+    href: "#",
+    status: "current",
+  },
+  {
+    name: "Business information",
+    description: "Penatibus eu quis ante.",
+    href: "#",
+    status: "upcoming",
+  },
+  {
+    name: "Theme",
+    description: "Faucibus nec enim leo et.",
+    href: "#",
+    status: "upcoming",
+  },
+  {
+    name: "Preview",
+    description: "Iusto et officia maiores porro ad non quas.",
+    href: "#",
+    status: "upcoming",
+  },
+];
+
+export default defineComponent({
+  setup() {
+    return { steps };
+  },
+});
+</script>
