8e462e2d16b0d9f98f79b620162873a2ec626935
twill <twill1@gmx.com>
2024-02-19 17:58:01 -0600

    Add tzdb package

--- vue/src/components/onboarding/TimeZone.vue
+++ vue/src/components/onboarding/TimeZone.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    <h5 class="font-bold tracking-widest text-slate-800">Timezone</h5>
+  <div class="text-left">
+    <div class="w-full mt-8 mb-6 ml-1 md:w-2/3 lg:w-1/2">
+      <h5 class="font-bold tracking-widest text-slate-800">Timezone</h5>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import { StepperStatusProp } from "../types";
+import { timeZonesNames } from "@vvo/tzdb";
 
 export default defineComponent({
   name: "TimeZone",
@@ -20,6 +23,7 @@ export default defineComponent({
 
   setup(props) {
     console.log(props.status);
+    console.log(timeZonesNames);
 
     return {};
   },
