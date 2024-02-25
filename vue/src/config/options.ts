ee1d22b1576f649595861445c2b764caf6e2e8af
twill <twill1@gmx.com>
2024-02-24 21:36:46 -0600

    Add currencies

--- vue/src/config/options.ts
+++ vue/src/config/options.ts
@@ -46,4 +46,111 @@ const availableCountries = [
   "United States",
 ];
 
-export { availableCountries };
+const availableCurrencies = [
+  {
+    value: "AUD",
+    text: "Australian Dollar",
+  },
+  {
+    value: "EUR",
+    text: "Euro",
+  },
+  {
+    value: "BRL",
+    text: "Brazilian Real",
+  },
+  {
+    value: "BGN",
+    text: "Bulgarian Lev",
+  },
+  {
+    value: "CAD",
+    text: "Canadian Dollar",
+  },
+  {
+    value: "HRK",
+    text: "Croatian Kuna",
+  },
+  {
+    value: "CZK",
+    text: "Czech Koruna",
+  },
+  {
+    value: "DKK",
+    text: "Danish Krone",
+  },
+  {
+    value: "GIP",
+    text: "Gibraltar Pound",
+  },
+  {
+    value: "HKD",
+    text: "Hong Kong Dollar",
+  },
+  {
+    value: "HUG",
+    text: "Hungarian Forint",
+  },
+  {
+    value: "INR",
+    text: "Indian Rupee",
+  },
+  {
+    value: "JPY",
+    text: "Japanese Yen",
+  },
+  {
+    value: "CHF",
+    text: "Swiss Franc",
+  },
+  {
+    value: "MYR",
+    text: "Malaysian Ringgit",
+  },
+  {
+    value: "MXN",
+    text: "Mexican Peso",
+  },
+  {
+    value: "NZD",
+    text: "New Zealand Dollar",
+  },
+  {
+    value: "NOK",
+    text: "Norwegian Krone",
+  },
+  {
+    value: "PLN",
+    text: "Polish Zloty",
+  },
+  {
+    value: "RON",
+    text: "Romanian Leu",
+  },
+  {
+    value: "SGD",
+    text: "Singapore Dollar",
+  },
+  {
+    value: "SEK",
+    text: "Swedish Krona",
+  },
+  {
+    value: "THB",
+    text: "Thai Baht",
+  },
+  {
+    value: "AED",
+    text: "United Arab Emirates Dirham",
+  },
+  {
+    value: "GBP",
+    text: "British Pound",
+  },
+  {
+    value: "USD",
+    text: "United States Dollar",
+  },
+];
+
+export { availableCountries, availableCurrencies };
