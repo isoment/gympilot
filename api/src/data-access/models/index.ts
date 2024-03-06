60c27989dbf9cb230f5b5b17932b84b8a5c572ec
twill <twill1@gmx.com>
2024-03-06 10:36:10 -0600

    Update relationships

--- api/src/data-access/models/index.ts
+++ api/src/data-access/models/index.ts
@@ -2,6 +2,9 @@ import User from "./user";
 import Role from "./role";
 import UserRole from "./userRole";
 import PasswordReset from "./passwordReset";
+import Organization from "./organization";
+import Location from "./location";
+import UserLocation from "./userLocation";
 
 /*********************************************************
  *  Here we can define the relationships between models  *
@@ -9,9 +12,21 @@ import PasswordReset from "./passwordReset";
 User.belongsToMany(Role, { through: UserRole, foreignKey: "user_id" });
 Role.belongsToMany(User, { through: UserRole, foreignKey: "role_id" });
 
+User.hasOne(Organization, { foreignKey: "owner_id" });
+Organization.belongsTo(User, { foreignKey: "owner_id" });
+
+Organization.hasMany(Location, { foreignKey: "organization_id" });
+Location.belongsTo(Organization, { foreignKey: "organization_id" });
+
+Location.belongsToMany(User, { through: UserLocation, foreignKey: "location_id" });
+User.belongsToMany(Location, { through: UserLocation, foreignKey: "user_id" });
+
 export default {
   User,
   Role,
   UserRole,
   PasswordReset,
+  Organization,
+  Location,
+  UserLocation,
 };
