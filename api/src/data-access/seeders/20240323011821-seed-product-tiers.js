dc5d5585492e365a436cc4e7ba8160a92e19c5c2
twill <twill1@gmx.com>
2024-03-23 13:39:00 -0500

    Update package.json files

--- api/src/data-access/seeders/20240323011821-seed-product-tiers.js
+++ api/src/data-access/seeders/20240323011821-seed-product-tiers.js
@@ -1,41 +1,9 @@
 "use strict";
+const { productTiers } = require("gympilot-shared-resources");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const productTiers = [
-      {
-        name: "Free",
-        price_usd_cents: 0,
-        max_active_members: 15,
-        max_locations: 1,
-      },
-      {
-        name: "Starter",
-        price_usd_cents: 1500,
-        max_active_members: 50,
-        max_locations: 1,
-      },
-      {
-        name: "Plus",
-        price_usd_cents: 5000,
-        max_active_members: 200,
-        max_locations: 5,
-      },
-      {
-        name: "Pro",
-        price_usd_cents: 10000,
-        max_active_members: 500,
-        max_locations: 10,
-      },
-      {
-        name: "Elite",
-        price_usd_cents: 20000,
-        max_active_members: 1000,
-        max_locations: 50,
-      },
-    ];
-
     for (const product of productTiers) {
       const existingRecord = await queryInterface.rawSelect(
         "product_tiers",
